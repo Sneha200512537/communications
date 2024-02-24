da0e89f6c953b0cc9726be9cd49cd0ce746702e8
Sneha200512537 <120434418+Sneha200512537@users.noreply.github.com>
2024-02-24 17:37:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function openTab(tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByTagName("section");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    var i;
+    var tabs = document.getElementsByTagName("section");
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
     }
     document.getElementById(tabName).style.display = "block";
 }
